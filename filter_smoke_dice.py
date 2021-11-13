d2a9692c321b88d3d3275caf9f587df20001989d
Alex_Egorov <ialex.egorov.01@gmail.com>
2021-11-13 16:26:32 +0300

    Add new photos

--- filter_smoke_dice.py
+++ filter_smoke_dice.py
@@ -21,6 +21,7 @@ from numpy.random import randn
 
 from keras.models import load_model
 
+from tensorflow.keras.optimizers import RMSprop,Adam
 
 import tensorflow as tf
 import tensorflow.keras
@@ -44,6 +45,7 @@ def dice_coef(y_true, y_pred, smooth=1):
     intersection = K.sum(y_true_f * y_pred_f)
     return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
 
+
 def dice_loss(y_true, y_pred):
     smooth = 1.
     y_true_f = K.flatten(y_true)
@@ -52,6 +54,7 @@ def dice_loss(y_true, y_pred):
     score = (2. * K.sum(intersection) + smooth) / \
         (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
     return 1. - score
+
 
 def bce_dice_loss(y_true, y_pred):
     return binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
@@ -71,15 +74,12 @@ def load_data(csv_file):
     for idx in range(n_samples):
         print(idx, ': ', data['IMAGE'].iloc[idx], ', mask: ', data['MASK'].iloc[idx])
 
-        img = load_rgb(data['IMAGE'].iloc[idx])
+        img = cv2.resize(load_rgb(data['IMAGE'].iloc[idx]), (640, 360))
         lx.append(img.astype('float32') / 255.)
 
         print(img.shape)
-        msk =  load_grayscale(data['MASK'].iloc[idx])
-
-        msk_r = cv2.resize(msk, (142, 80))
-
-        msk = np.expand_dims(msk_r, axis=-1)
+        msk = cv2.resize(load_grayscale(data['MASK'].iloc[idx]), (640, 360))
+        msk = np.expand_dims(msk, axis=-1)
         ly.append(msk.astype('float32') / 255.)
 
     size_i = lx[0].shape
@@ -120,22 +120,49 @@ def infer_image(model, img):
                                     
     return pred[0,:,:,0]    
 
+def return_graf(history):
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs = range(len(acc))
+
+    plt.plot(epochs, acc, 'g', label='Training accuracy')
+    plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+    plt.title('Training and validation accuracy')
+    plt.legend(loc=0)
+    plt.figure()
+    plt.show()
+
+    plt.plot(epochs, loss, 'r', label='Training loss')
+    plt.plot(epochs, val_loss, 'orange', label='Validation loss')
+    plt.title('Training and validation loss')
+
+    plt.legend(loc=0)
+    plt.figure()
+    plt.show()
+
+def check_rez(model, path="11.jpg"):
+    frame = load_rgb(path)
+    frame = cv2.resize(frame, (640, 360))
+    p_img = infer_image(model, frame)
+    image_show(frame)
+    image_show(p_img)
+
 def filter_model(input_shape, kernel_size, n_filters = 3):
 
     kernel_tuple = (kernel_size, kernel_size)
     
     input_img = Input(shape = input_shape)
     x = Conv2D(n_filters, kernel_tuple, activation='relu', padding='same')(input_img)
-    x = MaxPooling2D(pool_size = (3,3))(x)
-    x = Conv2D(n_filters*n_filters, kernel_tuple, activation='relu', padding='same')(x)
-    x = MaxPooling2D(pool_size = (3,3))(x)
-    x = Dense(1024, activation='relu')(x)
     x = Dropout(0.25)(x)
     output_img = Conv2D(1, kernel_tuple, activation='sigmoid', padding='same')(x)
 
     model = Model(input_img, output_img)
 
     model.compile(optimizer='adam', loss=bce_dice_loss, metrics=[dice_coef])
+    #model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
 
     model.summary()
     return model
@@ -143,50 +170,53 @@ def filter_model(input_shape, kernel_size, n_filters = 3):
 
 def train_filter(fit_bool, modelname = 'smokefilter_2conv3_11x11x3'):
 
-    input_shape = (720, 1280, 3)
+    #input_shape = (720, 1280, 3)
+    input_shape = (360, 640, 3)
     kernel_size = 11
 
     smokefilter = filter_model(input_shape, kernel_size, 3)
 
-    #h5name = modelname + '-weights_newest.h5'                                                                         
-    #smokefilter.load_weights(h5name)
+    h5name = modelname + '-weights.h5'                                                                         
+    smokefilter.load_weights(h5name)
 
     if(fit_bool==True):
         
         x_train, y_train = load_data('smoke_train.csv')
         x_valid, y_valid = load_data('smoke_valid.csv')
 
-
-        smokefilter.fit(x_train, y_train,
-                        epochs = 2,
+        history = smokefilter.fit(x_train, y_train,
+                        epochs = 40,
                         batch_size = 2,
                         shuffle = True,
                         validation_data = (x_train, y_train)
                        )
-        h5name = modelname + '-weights_newest.h5'
+
+        h5name = modelname + '-weights.h5'
         smokefilter.save_weights(h5name)
     
+        print(history)
+        #return_graf(history)
+
         with open(modelname + '-architecture.json', 'w') as f:
             f.write(smokefilter.to_json())
     else:
-        #frame = load_rgb("images/train/shot0015.png")
-        #p_img = infer_image(smokefilter, frame)
-        #image_show(frame)
-        #image_show(p_img)
-        cap = cv2.VideoCapture("./videos/1.mp4"); 
-        cap.set(3,1280) 
-        cap.set(4,700)  
+        check_rez(smokefilter, "images/s4/00008_000.jpg")
+        #check_rez(smokefilter)
 
-        while(cap.isOpened()):
-            ret, frame = cap.read()
-            if ret == True:
-              p_img = infer_image(smokefilter, frame)*100
-              cv2.imshow('Frame',p_img)
-              if cv2.waitKey(25) & 0xFF == ord('q'):
-                break
-            else:
-              break
+
+        #cap = cv2.VideoCapture("./videos/1.mp4"); 
+        #
+        #while(cap.isOpened()):
+        #    ret, frame = cap.read()
+        #    frame = cv2.resize(frame, (640, 360))
+        #    if ret == True:
+        #      p_img = infer_image(smokefilter, frame)*100
+        #      cv2.imshow('Frame',p_img)
+        #      if cv2.waitKey(25) & 0xFF == ord('q'):
+        #        break
+        #    else:
+        #      break
 
 
 if __name__ == '__main__':
-    train_filter(True, 'smokefilter_dice')
+    train_filter(False, 'smokefilter_dice')
